a76d7349e06282fecbf171e4a1a0e56be2d95e1f
martinsuasnavar <msuasnavar21@gmail.com>
2025-01-16 14:34:39 -0300

    Update navbar.jsx

--- src/app/components/containers/navbar.jsx
+++ src/app/components/containers/navbar.jsx
@@ -22,10 +22,10 @@ export default function NavBar() {
     return (
         <div className="font-semibold p-4 bg-black w-full fixed top-0">
             <div className="float-right mr-2 flex">
-                <div className="text-yellow-500 mr-2">
+                {/*<div className="text-yellow-500 mr-2">
                     {points !== null ? points : "0"}
                 </div>
-                <div>POINTS</div>
+                <div>POINTS</div>*/}
             </div>
         </div>
     );
