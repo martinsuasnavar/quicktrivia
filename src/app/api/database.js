ea63cc2ae7c61ef2b47a56d89d8103699f85c576
martinsuasnavar <msuasnavar21@gmail.com>
2025-01-16 13:49:42 -0300

    Update

--- src/app/api/database.js
+++ src/app/api/database.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 import fs from 'fs';
 import path from 'path';
 
-//import { env } from 'process';
+import { env } from 'process';
 const caCert = process.env.CA_CERT 
     ? Buffer.from(process.env.CA_CERT, 'base64').toString() 
     : null;
@@ -11,7 +11,6 @@ if (!caCert) {
     throw new Error('CA_CERT no está configurado en las variables de entorno.');
 }
 let connection
-
  export const connectToDatabase = async () => {
     if(!connection) {
         connection = await mysql.createConnection({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import  CreateSession  from "./components/supports/create-session";
 
 export default function Index() {
     CreateSession();
+    
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_NAME:', process.env.DB_NAME);
     return(
         <Home></Home>
     )
