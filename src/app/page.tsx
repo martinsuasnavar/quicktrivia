7a4ea2cf92fa7ab652a238c286d1800d9bb3f000
martinsuasnavar <msuasnavar21@gmail.com>
2025-01-16 13:55:26 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,12 @@ import  CreateSession  from "./components/supports/create-session";
 export default function Index() {
     CreateSession();
     
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_NAME:', process.env.DB_NAME);
+    console.log("DB_HOST:", process.env.DB_HOST);
+    console.log("DB_USER:", process.env.DB_USER);
+    console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "******" : "undefined");
+    console.log("DB_NAME:", process.env.DB_NAME);
+    console.log("DB_PORT:", process.env.DB_PORT);
+    
     return(
         <Home></Home>
     )
